29776b93d45ac6c54cbe7918b7d655617523fbd5
vinu-129 <vinay.chudasama@1rivet.local>
2019-10-09 07:08:45 -0700

    Final Commit

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,52 +1,66 @@
+var flag = 0;
+
+// Toggle menu
 function open_menu(layout) {
     if ($(window).width() <= 768) {
         if (layout == "add-menu") {
             $(".setting-menu").hide();
+            $('.theme-menu').hide();
+        }
+        else if(layout == "theme-menu"){
+            $(".setting-menu").hide();
+            $(".add-menu").hide();
         }
         else {
             $(".add-menu").hide();
+            $('.theme-menu').hide();
         }
     }
     $("." + layout).toggle(200);
 }
 
+// Change title value
 function change_val() {
     var text = $('#title_name').val();
     console.log(text);
     $("#content-title").text(text);
 }
 
-var flag = 0;
-
+// Nav toggle on click of hamburger icon
 $('.nav-toggle').click(function () {
+    if ($(window).width() >= 768) {
     if (flag == 0) {
         $('#site-nav').css("width", "250px");
         $('.nav-title').show();
         $('.add-menu').css("left", "250px");
+        $('.theme-menu').css("left", "250px");
         $('.nav-icon').css("width", "30px");
         $('.nav-link').css({ "justify-content": "flex-start" });
+        if ($(window).width() <= 768) {
+            $('#site-nav').css("position", "absolute");
+        }
+        else {
+         
+        }
         flag = 1;
     }
     else {
         $('#site-nav').css("width", "60px");
         $('.nav-title').hide();
         $('.add-menu').css("left", "60px");
-
+        $('.theme-menu').css("left", "60px");
         flag = 0;
     }
+}
 })
 
-
-
+// Closing menu
 $('.close-menu').click(function () {
     $('.add-menu').hide(200);
+    $('.theme-menu').hide(200);
 })
 
-
-// $('.collapse').click(function () {
-//     $('.collapse-body').slideToggle('slow');
-// });
-
+// Collapsible menu
 $('.collapse-title').click(function (e) {
     e.preventDefault();
     var $this = $(this);
@@ -64,5 +78,11 @@ $('.collapse-title').click(function (e) {
         $this.children(".collapse-icon").addClass("fa-minus");
         flag = 0;
     }
+});
 
+// Custom theme
+$('.menu-list-item').click(function () {
+    var text = $(this).children('.menu-list-title').text();
+    console.log(text);
+    $(".bg-custom").css("background-color", text);
 });
